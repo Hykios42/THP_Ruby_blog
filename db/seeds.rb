11e62d414796216275e466a995705ffcd120b560
hydref <charles.dutheil@gmail.com>
2019-01-30 22:41:00 +0100

    finishBlog

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |index|
+  user = User.create!(first_name: Faker::Company.name, last_name: Faker::Name.last_name, email: "email#{index}@example.com")
+end
+
+5.times do
+ category = Category.create!(name: Faker::LordOfTheRings.character)
+end
+
+10.times do |index|
+  article = Article.create!(title: Faker::Company.name, content: Faker::Shakespeare.hamlet_quote, user_id: index + 1, category_id: rand(1..5))
+end
+
+15.times do
+ comment = Comment.create!(content: Faker::Shakespeare.hamlet_quote, user_id: rand(1..10), article_id: rand(1...10))
+end
+
+15.times do
+ like = Like.create!(user_id: rand(1..10), article_id: rand(1...10))
+end
